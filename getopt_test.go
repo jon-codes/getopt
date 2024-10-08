e40bc2e815c3200cf2bbb7565cf3a2f9b61d9d6e
Jon Webb <jon@jonwebb.dev>
2024-10-08 17:56:06 -0400

    unicode support for options

--- getopt_test.go
+++ getopt_test.go
@@ -29,9 +29,32 @@ func TestOptStr(t *testing.T) {
 			t.Errorf("got %+v, but wanted %+v", got, want)
 		}
 	})
+
+	t.Run("it parses multi-byte opts", func(t *testing.T) {
+		got := OptStr(`αβ:γ::δ:ε`)
+		want := []Opt{
+			{Char: 'α', HasArg: NoArgument},
+			{Char: 'β', HasArg: RequiredArgument},
+			{Char: 'γ', HasArg: OptionalArgument},
+			{Char: 'δ', HasArg: RequiredArgument},
+			{Char: 'ε', HasArg: NoArgument},
+		}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %+v, but wanted %+v", got, want)
+		}
+	})
 }
 
 func TestLongOptStr(t *testing.T) {
+	t.Run("it handles empty values", func(t *testing.T) {
+		got := LongOptStr(``)
+
+		if len(got) != 0 {
+			t.Errorf("got length %d, but wanted 0", len(got))
+		}
+	})
+
 	t.Run("it parses long opts", func(t *testing.T) {
 		got := LongOptStr(`long_a,long_b:,long_c::`)
 		want := []LongOpt{
@@ -44,15 +67,17 @@ func TestLongOptStr(t *testing.T) {
 			t.Errorf("got %+v, but wanted %+v", got, want)
 		}
 	})
-}
 
-func TestArgsStr(t *testing.T) {
-	t.Run("it parses args strings", func(t *testing.T) {
-		got := argsStr(`arg1 'arg2a "arg2b"' "arg3a 'arg3b'"`)
-		want := []string{"arg1", "arg2a \"arg2b\"", "arg3a 'arg3b'"}
+	t.Run("it parses long opts with multi-byte elements", func(t *testing.T) {
+		got := LongOptStr(`άλφα,βήτα:,γάμμα::`)
+		want := []LongOpt{
+			{Name: "άλφα", HasArg: NoArgument},
+			{Name: "βήτα", HasArg: RequiredArgument},
+			{Name: "γάμμα", HasArg: OptionalArgument},
+		}
 
 		if !slices.Equal(got, want) {
-			t.Errorf("got %+q, but wanted %+q", got, want)
+			t.Errorf("got %+v, but wanted %+v", got, want)
 		}
 	})
 }
@@ -92,13 +117,31 @@ func TestStateReset(t *testing.T) {
 func TestGetOpt(t *testing.T) {
 	function := FuncGetOpt
 
-	t.Run("it parses short opts", func(t *testing.T) {
-		s := NewState(argsStr(`prgm -a p1`))
-		p := Params{Opts: OptStr(`a`), Function: function}
+	t.Run("it parses opts", func(t *testing.T) {
+		s := NewState(argsStr(`prgm -abc -d p1`))
+		p := Params{Opts: OptStr(`abcd`), Function: function}
 
 		wants := []assertion{
-			{char: 'a', args: argsStr(`prgm -a p1`), optIndex: 2},
-			{err: ErrDone, args: argsStr(`prgm -a p1`), optIndex: 2},
+			{char: 'a', args: argsStr(`prgm -abc -d p1`), optIndex: 1},
+			{char: 'b', args: argsStr(`prgm -abc -d p1`), optIndex: 1},
+			{char: 'c', args: argsStr(`prgm -abc -d p1`), optIndex: 2},
+			{char: 'd', args: argsStr(`prgm -abc -d p1`), optIndex: 3},
+			{err: ErrDone, args: argsStr(`prgm -abc -d p1`), optIndex: 3},
+		}
+
+		assertSeq(t, s, p, wants)
+	})
+
+	t.Run("it parses multi-byte opts", func(t *testing.T) {
+		s := NewState(argsStr(`prgm -αβγ -δ p1`))
+		p := Params{Opts: OptStr(`αβγδ`), Function: function}
+
+		wants := []assertion{
+			{char: 'α', args: argsStr(`prgm -αβγ -δ p1`), optIndex: 1},
+			{char: 'β', args: argsStr(`prgm -αβγ -δ p1`), optIndex: 1},
+			{char: 'γ', args: argsStr(`prgm -αβγ -δ p1`), optIndex: 2},
+			{char: 'δ', args: argsStr(`prgm -αβγ -δ p1`), optIndex: 3},
+			{err: ErrDone, args: argsStr(`prgm -αβγ -δ p1`), optIndex: 3},
 		}
 
 		assertSeq(t, s, p, wants)
@@ -109,18 +152,7 @@ func TestGetOpt(t *testing.T) {
 		p := Params{Opts: OptStr(`b`), Function: function}
 
 		wants := []assertion{
-			{char: 'a', err: ErrUnknownOpt, args: argsStr(`prgm -a`), optIndex: 2},
-		}
-
-		assertSeq(t, s, p, wants)
-	})
-
-	t.Run("it errors on illegal opts", func(t *testing.T) {
-		s := NewState(argsStr(`prgm -π`))
-		p := Params{Opts: OptStr(`π`), Function: function}
-
-		wants := []assertion{
-			{char: 'π', err: ErrUnknownOpt, args: argsStr(`prgm -π`), optIndex: 2},
+			{char: 'a', err: ErrUnknownOpt, args: argsStr(`prgm -a`), optIndex: 1},
 		}
 
 		assertSeq(t, s, p, wants)
@@ -279,7 +311,7 @@ func TestGetOpt(t *testing.T) {
 		p := Params{LongOpts: LongOptStr(`longa`), Function: function}
 
 		wants := []assertion{
-			{char: '-', err: ErrUnknownOpt, args: argsStr(`prgm --longa`), optIndex: 2},
+			{char: '-', err: ErrUnknownOpt, args: argsStr(`prgm --longa`), optIndex: 1},
 		}
 
 		assertSeq(t, s, p, wants)
@@ -290,7 +322,7 @@ func TestGetOpt(t *testing.T) {
 		p := Params{LongOpts: LongOptStr(`longa`), Function: function}
 
 		wants := []assertion{
-			{char: 'l', err: ErrUnknownOpt, args: argsStr(`prgm -longa`), optIndex: 2},
+			{char: 'l', err: ErrUnknownOpt, args: argsStr(`prgm -longa`), optIndex: 1},
 		}
 
 		assertSeq(t, s, p, wants)
@@ -324,12 +356,26 @@ func TestGetOptLong(t *testing.T) {
 		assertSeq(t, s, p, wants)
 	})
 
+	t.Run("it parses long opts with multi-byte elements", func(t *testing.T) {
+		s := NewState(argsStr(`prgm --άλφα --βήτα --γάμμα`))
+		p := Params{LongOpts: LongOptStr(`άλφα,βήτα,γάμμα`), Function: function}
+
+		wants := []assertion{
+			{name: "άλφα", args: argsStr(`prgm --άλφα --βήτα --γάμμα`), optIndex: 2},
+			{name: "βήτα", args: argsStr(`prgm --άλφα --βήτα --γάμμα`), optIndex: 3},
+			{name: "γάμμα", args: argsStr(`prgm --άλφα --βήτα --γάμμα`), optIndex: 4},
+			{err: ErrDone, args: argsStr(`prgm --άλφα --βήτα --γάμμα`), optIndex: 4},
+		}
+
+		assertSeq(t, s, p, wants)
+	})
+
 	t.Run("it does not parse long opts with '-' prefix", func(t *testing.T) {
 		s := NewState(argsStr(`prgm -longa`))
 		p := Params{LongOpts: LongOptStr(`longa`), Function: function}
 
 		wants := []assertion{
-			{char: 'l', err: ErrUnknownOpt, args: argsStr(`prgm -longa`), optIndex: 2},
+			{char: 'l', err: ErrUnknownOpt, args: argsStr(`prgm -longa`), optIndex: 1},
 		}
 
 		assertSeq(t, s, p, wants)
@@ -443,13 +489,13 @@ func assertFixture(t testing.TB, f fixture) {
 		}
 
 		if res.Char != want.Char {
-			t.Errorf("iter %d, wanted Char %q, but got %q", iter, res.Char, want.Char)
+			t.Errorf("iter %d, got Char %q, but wanted %q", iter, res.Char, want.Char)
 		}
 		if res.Name != want.Name {
-			t.Errorf("iter %d, wanted Name %q, but got %q", iter, res.Name, want.Name)
+			t.Errorf("iter %d, got Name %q, but wanted %q", iter, res.Name, want.Name)
 		}
 		if res.OptArg != want.OptArg {
-			t.Errorf("iter %d, wanted OptArg %q, but got %q", iter, res.OptArg, want.OptArg)
+			t.Errorf("iter %d, got OptArg %q, but wanted %q", iter, res.OptArg, want.OptArg)
 		}
 	}
 
